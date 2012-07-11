f0877c7cc6bad15d994037eb52b87f5569f51611
Adrienne Peirce <adrienne.peirce@gmail.com>
2012-07-10 20:15:49 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartless::Application.config.secret_token = 'c6346ea237051c9fb73ab735250e0fa9863d3b21bd959301e32d916887213d1d179fb1aee2e9457cb119cd612c297320d478f4cf39ed750ddfebde2426db4b80'
